f6f8e3746669b70eb6550c41f4d30d38a6eb978f
Ali Bouajila <alibouajila18@gmail.com>
2025-04-06 00:31:58 +0100

    adding user model

---
+++ server/models/user.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+const userSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    match: /^\S+@\S+\.\S+$/, 
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ['doctor', 'assistant'],
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+export default User;
--- server/routes/patient.js
+++ server/routes/patient.js
@@ -3,7 +3,7 @@ import Patient from '../models/Patient.js';
 
 const router = express.Router();
 
-// Create a new patient
+// Create a new patient (just for test )
 router.post('/register', async (req, res) => {
   try {
     const newPatient = new Patient(req.body);
@@ -47,5 +47,22 @@ router.delete('/:id', async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 });
-
+// Get all verified patients
+router.get('/verified', async (req, res) => {
+    try {
+      const verifiedPatients = await Patient.find({ isVerified: true });
+      res.status(200).json(verifiedPatients);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  });
+  // Get all unverified patients
+router.get('/unverified', async (req, res) => {
+    try {
+      const verifiedPatients = await Patient.find({ isVerified: false });
+      res.status(200).json(verifiedPatients);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  });
 export default router;
