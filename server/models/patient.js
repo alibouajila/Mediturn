689b2eb11e3970a974005a117597275913ca22c6
Ali Bouajila <alibouajila18@gmail.com>
2025-04-05 23:59:36 +0100

    adding patient model

---
+++ server/models/patient.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+const patientSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  CIN: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 8,
+    maxlength: 8,
+  },
+  dateOfBirth: {
+    type: Date,
+    required: true,
+  },
+  gender: {
+    type: String,
+    enum: ['Male', 'Female', 'Other'],
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+    match: /^[0-9]{8,13}$/,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  }
+});
+
+const Patient = mongoose.model('Patient', patientSchema);
+export default Patient;
