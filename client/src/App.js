d9da68ad354dfbf28be916576d04a58848d5d7f3
Ali Bouajila <alibouajila18@gmail.com>
2025-04-09 23:37:09 +0100

    Handling home page

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
-
+import Rendezvous from './pages/Rendezvous';
 function App() {
   return (
     <Router>
@@ -11,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/rendezvous" element={<Rendezvous />} />
       </Routes>
     </Router>
   );
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import './Home.css'
 
 function Home() {
+  const navigate=useNavigate()
+  const handleClick=()=>{
+    navigate('/rendezvous')
+  }
+  
+  
   return (
-    <div>Home</div>
+    <div className="home-container">
+      <div className="home-content">
+        <h1>Bienvenue chez Mediturn</h1>
+        <p>Réservez facilement votre consultation en ligne</p>
+        <button onClick={()=>handleClick()} className="rendezvous-button">Prenez un rendez-vous</button>
+      </div>
+    </div>
   )
 }
 
