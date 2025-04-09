29556e3d250e04f25f5653154d6afc376a15bf62
Ali Bouajila <alibouajila18@gmail.com>
2025-04-09 23:06:19 +0100

    Fixing nav design

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
 
-function Navbar() {
+const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <header className="navbar">
+      <div className="navbar-container">
+        <div><Link to="/" className="navbar-logo">Mediturn</Link> </div>
+        <ul className="navbar-links">
+          <li><Link to="/" className="navbar-link">Home</Link></li>
+          <li><Link to="/about" className="navbar-link">About Us</Link></li>
+        </ul>
+      </div>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
