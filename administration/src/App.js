abd0f5bc6090c1b3c30838d32bc7df943b66b984
Ali Bouajila <alibouajila18@gmail.com>
2025-04-11 10:55:57 +0100

    Admininstration dashboard

---
+++ administration/src/App.js
@@ -0,0 +1,18 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to the Administration App</h1>
+      </header>
+      <main>
+        <p>main </p>
+      </main>
+      <footer>
+        <p>&copy; 2023 Administration App</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
