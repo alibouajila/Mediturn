e521ecaf2544ca1d49d03d47923d8590e0963254
Ali Bouajila <alibouajila18@gmail.com>
2025-04-12 16:10:19 +0100

    Handling home page

--- administration/src/pages/Home.jsx
+++ administration/src/pages/Home.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css';
 
-function Home() {
+const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home-container">
+      <div className="home-content">
+        <h1>Welcome to <span>Mediturn</span></h1>
+        <p>Your smart solution for managing medical appointments and patient flow.</p>
+        <div className="home-buttons">
+          <Link to="/signup" className="btn primary-btn">Sign Up</Link>
+          <Link to="/login" className="btn secondary-btn">Log In</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
